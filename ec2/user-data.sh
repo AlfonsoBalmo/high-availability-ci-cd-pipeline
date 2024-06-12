f9d457146bc40fd3ae4e28129853dd29d9b03650
Alfonso <alfonso.balmori@skydropx.com>
2024-06-11 23:17:27 -0600

    subnet

--- ec2/user-data.sh
+++ ec2/user-data.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # Actualizar el sistema
-yum update -y
-
-# Instalar Docker
-yum install -y docker
+sudo yum update -y
+sudo amazon-linux-extras install docker
 
 # Iniciar Docker
-service docker start
+sudo service docker start
+sudo usermod -a -G docker ec2-user
 
-# Agregar el usuario ec2-user al grupo docker
-usermod -a -G docker ec2-user
+# Clonar el repositorio y ejecutar la aplicación
+git clone https://github.com/AlfonsoBalmo/high-availability-hiberus-challenge.git /home/ec2-user/app
+cd /home/ec2-user/app
 
-# Descargar e iniciar la imagen de Docker de la aplicación
-docker run -d -p 80:80 471112872744.dkr.ecr.us-east-1.amazonaws.com/hiberus-repository:latest
+# Esperar a que la base de datos esté lista
+while ! nc -z ${db_endpoint} 5432; do
+  sleep 1
+done
 
+# Ejecutar el contenedor Docker
+sudo docker run -d -p 80:80 -e DATABASE_URL=postgres://user:3216540@${db_endpoint}:5432/mydb your-aws-account-id.dkr.ecr.us-east-1.amazonaws.com/hiberus-repository:latest
